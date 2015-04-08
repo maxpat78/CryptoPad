b71af20de8ba46af54ae11cc327b839182bab74a
maxpat78 <maxpat78@yahoo.it>
2015-04-08 07:21:17 +0200

    Upgraded _libeay.c
    - bug with Py_BuildValue in Python 3.4 fixed
    - added CTR byte swapping for Big Endian architectures

--- _libeay.c
+++ _libeay.c
@@ -1,53 +1,70 @@
 #include "Python.h"
 #include <openssl/aes.h>
 
+#ifdef BYTE_ORDER_1234
+void betole64(unsigned long long *x) {
+*x = (*x & 0x00000000FFFFFFFF) << 32 | (*x & 0xFFFFFFFF00000000) >> 32;
+*x = (*x & 0x0000FFFF0000FFFF) << 16 | (*x & 0xFFFF0000FFFF0000) >> 16;
+*x = (*x & 0x00FF00FF00FF00FF) << 8  | (*x & 0xFF00FF00FF00FF00) >> 8;
+}
+#endif
+
 /*
 Nel modo CTR il cifrato risulta dallo XOR tra ciascun blocco di testo in chiaro
-e un contatore cifrato in modo ECB realizzato, preferibilmente, mediante unione
+e un contatore cifrato in modo ECB, realizzato, preferibilmente, mediante unione
 di n bit casuali con n bit di contatore.
 
-I protocolli AE-1 e AE-2 di WinZip richiedono che il contatore sia un numero a
-128 bit codificato in Little Endian diversamente dalle maggiori implementazioni
-in Big Endian; inoltre il contatore parte da 1 senza alcun contenuto casuale.
-
-NOTA: la versione in Python con ctypes è circa 72 volte più lenta!
-*/
+Il protocollo AE di WinZip richiede che il contatore sia un numero a 128 bit
+codificato Little Endian diversamente dalle maggiori implementazioni: esso
+parte da 1, senza alcun contenuto casuale. */
 static PyObject *
 p_AES_ctr128_le_crypt(self, args)
 PyObject *self, *args;
 {
-	char *s, *key, *out_buf;
-	unsigned int s_len, key_len, i, j;
-	char ctr_counter_le[16], *ctr_encrypted_counter;
-	unsigned long long *p;
+	char ctr_counter_le[16];
+	char ctr_encrypted_counter[16];
+	char* p = ctr_encrypted_counter;
+	char* q = p+8;
+	char *key, *buf, *pbuf;
+	unsigned int key_len, buf_len, i;
 	AES_KEY aes_key;
 
-	if (!PyArg_ParseTuple(args, "s#s#", &key, &key_len, &s, &s_len)) return 0;
-
-	if (AES_set_encrypt_key(key, key_len*8, &aes_key) < 0)
-		return Py_BuildValue("z", 0);
+	if ( !PyArg_ParseTuple(args, "s#s#", &key, &key_len, &buf, &buf_len) ||
+		AES_set_encrypt_key(key, key_len*8, &aes_key) < 0 )
+		return Py_BuildValue("s", NULL);
 
 	memset(ctr_counter_le, 0, 16);
-	ctr_encrypted_counter = out_buf = malloc(s_len);
-	p = (unsigned long long*) s;
 
-	for (i=0; i < s_len/16; i++) {
-		(*((unsigned int*) ctr_counter_le))++;
+	/* Lavora su una copia del buffer originale */
+	pbuf = PyMem_Malloc(buf_len);
+	memcpy(pbuf, buf, buf_len);
+	buf = pbuf;
+
+	for (i=0; i < buf_len/16; i++) {
+#ifdef BYTE_ORDER_1234
+		betole64(&ctr_counter_le);
+#endif
+		(*((unsigned long long*) ctr_counter_le))++;
 		AES_ecb_encrypt(ctr_counter_le, ctr_encrypted_counter, &aes_key, 1);
-		*((unsigned long long*) ctr_encrypted_counter)++ ^= *p++;
-		*((unsigned long long*) ctr_encrypted_counter)++ ^= *p++;
+		*((unsigned long long*) buf)++ ^= *((unsigned long long*) p);
+		*((unsigned long long*) buf)++ ^= *((unsigned long long*) q);
 	}
 
-	j = s_len%16;
-	if (j) {
-		(*((unsigned int*) ctr_counter_le))++;
+	if ((i = buf_len%16)) {
+#ifdef BYTE_ORDER_1234
+		betole64(&ctr_counter_le);
+#endif
+		(*((unsigned long long*) ctr_counter_le))++;
 		AES_ecb_encrypt(ctr_counter_le, ctr_encrypted_counter, &aes_key, 1);
-		for (i=0; i < j; i++) {
-			ctr_encrypted_counter[i] ^= ((char*)p)[i];
-		}
+		while (i--)
+			*buf++ ^= *p++;
 	}
 
-	return Py_BuildValue("s#", out_buf, s_len);
+#if PY_MAJOR_VERSION > 2
+	return Py_BuildValue("y#", pbuf, buf_len);
+#else
+	return Py_BuildValue("s#", pbuf, buf_len);
+#endif
 }
 
 
